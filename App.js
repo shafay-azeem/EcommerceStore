5bb202ebee56ee612b83358368aaff1eefa37703
shafay-azeem <shafayazeem12@gmail.com>
2022-09-30 15:11:50 +0500

    updated

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Header from './src/components/Layout/Header'
+import Banner from './src/components/Home/Banner'
 
 
 export default function App() {
   return (
     <View>
       <Header></Header>
+      <Banner></Banner>
     </View>
   )
 }

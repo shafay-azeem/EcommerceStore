da9fe06d5c0f4e8bdd38df593645a921a0fb078c
faizan-99 <mfaizy605@gmail.com>
2022-10-01 16:54:18 +0500

    remove packages material ui

--- src/components/Home/ProductCard.js
+++ src/components/Home/ProductCard.js
@@ -1,53 +1,53 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    Dimensions,
-    Image,
-    TouchableOpacity,
-    TouchableWithoutFeedback,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-var { width } = Dimensions.get('window')
-const ProductCard = ({ products }) => {
-    return (
-        <View style={styles.ProductCard}>
-            <Image source={{ uri: products.images[0].url }} style={styles.image} />
-            <Text>{products.name}</Text>
-        </View>
-    )
-}
+var {width} = Dimensions.get('window');
+const ProductCard = ({products}) => {
+  return (
+    <View style={styles.ProductCard}>
+      <Image source={{uri: products.images[0].url}} style={styles.image} />
+      <Text>{products.name}</Text>
+    </View>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
 
 const styles = StyleSheet.create({
-    ProductCard: {
-        width: width / 2 - 30,
-        height: width / 1.7,
-        borderRadius: 10,
-        elevation: 8,
-        backgroundColor: '#e5e5e5',
-        flexWrap: 'wrap',
-        margin: 10,
-    },
-    image: {
-        width: '100%',
-        height: width / 2 - 60,
-        resizeMode: 'contain',
-        borderTopRightRadius: 10,
-        borderTopLeftRadius: 10,
-    },
-    outOfStock: {
-        width: 50,
-        height: 50,
-        backgroundColor: 'red',
-        borderRadius: 50,
-        position: 'absolute',
-        top: -10,
-        left: -10,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
+  ProductCard: {
+    width: width / 2 - 30,
+    height: width / 1.7,
+    borderRadius: 10,
+    elevation: 8,
+    backgroundColor: '#e5e5e5',
+    flexWrap: 'wrap',
+    margin: 10,
+  },
+  image: {
+    width: '100%',
+    height: width / 2 - 60,
+    resizeMode: 'contain',
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+  outOfStock: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
+    borderRadius: 50,
+    position: 'absolute',
+    top: -10,
+    left: -10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

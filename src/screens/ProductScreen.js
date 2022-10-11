ef716e971cba13741919dce3cb6babca5af4f119
faizan-99 <mfaizy605@gmail.com>
2022-10-11 20:16:05 +0500

    updated

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,16 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import Header from "../components/Layout/Header"
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import Header from '../components/Layout/Header';
+import React from 'react';
+import FilterProducts from '../components/Products/FilterProducts';
 
-const ProductScreen = ({ navigation }) => {
-    return (
-        <View>
-            <Header navigation={navigation}></Header>
-            <Text>ProductScreen</Text>
-        </View>
-    )
-}
+const ProductScreen = ({navigation}) => {
+  return (
+    <View>
+      <Header navigation={navigation}></Header>
+      <Text>ProductScreen</Text>
+      <FilterProducts navigation={navigation} />
+    </View>
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
